ff103a12c93515b8d9bff5e06205ee93bc18e7b3
Stephanie Ritzer <55722805+slritzer@users.noreply.github.com>
2019-10-25 17:58:08 -0700

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	//Console output of HelloWorld!
+    cout << "Hello World!";
+
+	return 0;
+}
